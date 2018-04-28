5d8ab0e95be1b834a6de906470ea95ced8befedd
wuqiong <wuqiong>
2018-04-28 12:06:50 +0800

    original

---
+++ app/src/main/java/com/qy/demo/activity/ReboundActivity.java
@@ -0,0 +1,14 @@
+package com.qy.demo.activity;
+
+import android.os.Bundle;
+
+import com.qy.demo.R;
+
+public class ReboundActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rebound);
+    }
+}
